d6642f3b161eb90326bf00fa91fbea3bb15b71f8
Martinhache475 <108502607+Martinhache475@users.noreply.github.com>
2022-07-21 15:19:40 -0300

    Update scritp.js

--- pages&style/scritp.js
+++ pages&style/scritp.js
@@ -7,6 +7,11 @@ let space = ("<br>");
 document.write(space);
 document.write(space);
 document.write(space);
+
 if (CalcIMC <18) {document.write ("Su condicion es: " + "Bajo peso");} else {
      if (CalcIMC >18 && CalcIMC<25) {document.write ("Su condicion es: " + "Normal");} else {
-          if (CalcIMC > 25) {document.write("Su condicion es: " + "Sobrepeso")}}}
+          if (CalcIMC > 25) {document.write("Su condicion es: " + "Sobrepeso")}
+            if (CalcIMC >30 && CalcIMC<35) {document.write(" Tipo: " + "Obesidad I")} else {
+                if (CalcIMC >35 && CalcIMC<40) {document.write(" Tipo: " + "Obesidad II")} else {if (CalcIMC >41 && CalcIMC<50) {document.write(" Tipo: " + "Obesidad III")} else {
+                    if (CalcIMC >50) {document.write(" Tipo: " + "Nah")}}}}}}
+                
